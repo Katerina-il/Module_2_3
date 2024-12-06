dace2cd7fd042544032f29712d7d6d154c3a6728
Katerina-il <kkaterina.iln@gmail.com>
2024-12-06 14:49:52 +0300

    Add files via upload

---
+++ module_2_3_6SrkjLb.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list_len = len(my_list)
+n = 0
+ind_list = my_list[0]
+while my_list[n] >= 0 and n <= my_list_len:
+    if my_list[n] >= 0:
+        print(my_list[n])
+        n = n + 1
+    continue
+    break
+
+
+
